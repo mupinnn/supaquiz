414c65248ee5b884564a34d345a1cd2ee23a946b
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-08-27 21:25:57 +0700

    chore: load assets with built-in asset modules

--- webpack.common.js
+++ webpack.common.js
@@ -8,6 +8,18 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "SupaQuiz",
